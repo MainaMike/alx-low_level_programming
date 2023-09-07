5738621c947b8c1f335979edc69fe3c17c78f6cb
MainaMike <mwachira144@gmail.com>
2023-09-07 08:34:07 -0700

    HELLO WORLD

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main-entry point
+ *return-If successful, non-negative value is returned.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
