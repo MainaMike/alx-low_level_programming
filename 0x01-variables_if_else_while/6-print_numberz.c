daf9677fd8095b74c67186b06ffd9d688acec911
MainaMike <mwachira144@gmail.com>
2023-09-08 07:35:06 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that prints numbers of base 10 from 0.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
