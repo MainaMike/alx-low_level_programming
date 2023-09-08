8bfd086c9e0a99df3980b2b41a9a51a88b74458d
MainaMike <mwachira144@gmail.com>
2023-09-08 07:19:17 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all numbers of base 10 starting from 0.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d\n", num);
+	}
+
+	return (0);
+}
