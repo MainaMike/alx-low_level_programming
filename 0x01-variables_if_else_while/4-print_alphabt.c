c8d5309ddf22a3045a509090ab5cbde5482cc7c0
MainaMike <mwachira144@gmail.com>
2023-09-08 07:06:13 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabets in lowercase except q and e
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != q || e)
+		{
+			putchar(alpha);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
