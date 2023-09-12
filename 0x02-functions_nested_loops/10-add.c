474592f36bc8b1672f490470fffc27461b1374e3
MainaMike <mwachira144@gmail.com>
2023-09-12 07:52:52 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds two integers
+ *
+ * @num1: 1st number
+ *
+ * @num2: 2nd number
+ *
+ * Return: sum of the two.
+ */
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of a number.
+ *
+ * @num - Number being checked.
+ *
+ * Return: Last digit of the number.
+ */
+int print_last_digit(int num)
+{
+	int result;
+
+	result = num % 10;
+
+	return (result);
+}
