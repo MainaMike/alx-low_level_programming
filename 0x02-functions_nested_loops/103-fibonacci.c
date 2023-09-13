ad7ebfcb8fc75c33918ba14e13a163841a627185
MainaMike <mwachira144@gmail.com>
2023-09-13 06:54:06 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints fibanacci series
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total_sum;
+
+	while (sum < 4000000)
+	{
+		sum = fib1 + fib2;
+
+		if ((sum % 2) == 0)
+		{
+			total_sum += sum;
+		}
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+
+	printf("%.0f\n", total_sum);
+
+	return (0);
+}
