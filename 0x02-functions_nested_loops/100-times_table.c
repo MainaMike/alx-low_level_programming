856d93e1c7a60e25f2b9bd62162e941987210462
MainaMike <mwachira144@gmail.com>
2023-09-13 09:22:22 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,28 +8,45 @@ void print_times_table(int n)
 {
 	int i, j, result;
 
-	if (n < 0 || n > 15)
+	if (n >= 0 && n <= 14)
 	{
-		return;
-	}
-
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			result = i * j;
-
-			_putchar(result);
-
-			if  (j < n)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				result = i * j;
+
+				if ( result > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else if (result > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else
+				{
+					if (j != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+
+					_putchar(result + '0');
+				}
 			}
-			else
-			{
-				_putchar('\n');
-			}
+
+			_putchar('\n');
 		}
 	}
 }
