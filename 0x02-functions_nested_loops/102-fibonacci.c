d2b639bff73d4d8ec4361ad7640c7fd91d65443b
MainaMike <mwachira144@gmail.com>
2023-09-13 06:42:08 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints first 50 Fibonacci numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int counter;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (counter = 0; counter < 50; counter++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (counter == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+	}
+
+	return (0);
+}
