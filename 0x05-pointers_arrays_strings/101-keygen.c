a68b5df70122377a12bd9b444a3c18e5a91d45d9
MainaMike <mwachira144@gmail.com>
2023-09-18 10:24:48 -0700

    Task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,34 +1,51 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
 /**
  * main - generates random valid passwords
- *
- * Return: 0 (Success)
+ * Return: Always 0
  */
-
-#define PASSWORD_LENGTH 64
-
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
-    const char valid_characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int i;
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-    // Seed the random number generator with the current time
-    srand(time(NULL));
+	srand(time(0));
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+	password[index] = '\0';
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
 
-    // Generate a random password
-    for (i = 0; i < PASSWORD_LENGTH; i++)
-    {
-        int index = rand() % (sizeof(valid_characters) - 1); // Get a random index
-        password[i] = valid_characters[index];
-    }
+		if ((sum - 2772) % 2 != 0)
+		{
+			diff_half1++;
+		}
 
-    password[PASSWORD_LENGTH] = '\0'; // Null-terminate the password
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				 password[index] -= diff_half1;
+				 break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+	printf("%s", password);
 
-    printf("%s\n", password);
-
-    return (0);
+	return (0);
 }
-
