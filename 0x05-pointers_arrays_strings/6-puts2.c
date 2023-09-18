adf1e518f825971da5f56a4c4db0db8099db9e81
MainaMike <mwachira144@gmail.com>
2023-09-18 05:18:49 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string.
+ *
+ * @str: string being checked.
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int counter = 0;
+
+	while (str[counter] != '\0')
+	{
+		_putchar(str[counter]);
+		counter += 2;
+	}
+
+	_putchar('\n');
+}
