c1d0881033b000c1e1394ba1dcaa90c4e1bb5da6
MainaMike <mwachira144@gmail.com>
2023-09-18 02:02:02 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string being printed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int counter;
+	int num;
+
+	for (counter = 0; s[counter] != '\0'; counter++)
+	{
+		length = counter + 1;
+	}
+	for (num = length; num >= 0; num--)
+	{
+		_putchar(s[num]);
+	}
+}
