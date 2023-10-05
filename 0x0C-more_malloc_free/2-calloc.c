ae1cb34b8319f5d2c3e37d508afa53749bf9de8e
MainaMike <mwachira144@gmail.com>
2023-10-05 06:15:57 -0700

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * @nmemb: number of elements in array
+ * @size: size of bytes.
+ *
+ * Return: NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		*((char *)ptr + i) = 0;
+	}
+
+	return (ptr);
+}
